75e1134709d34384fc671d6fa701334fa5d12978
EphriamHab <ephremhabtamu2015@gmail.com>
2024-10-03 11:21:40 +0300

    colors

---
+++ lib/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFF09133A);
+const secondaryColor = Color(0xFFE5E5E5);
+const accentColor = Color(0xFFE5E5E5);
+const errorColor = Color(0xFFD32F2F);
+const successColor = Color(0xFF4CAF50);
+const warningColor = Color(0xFFFFA000);
