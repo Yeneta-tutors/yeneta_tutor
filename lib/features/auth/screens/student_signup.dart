6307446690a95bd1db3e16a4e0e660513a59eba6
EphriamHab <ephremhabtamu2015@gmail.com>
2024-10-04 20:16:31 +0300

    student  signup

--- lib/features/auth/screens/student_signup.dart
+++ lib/features/auth/screens/student_signup.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../controllers/signup_controller.dart';
 import 'package:yeneta_tutor/models/student_model.dart';
+import 'package:yeneta_tutor/features/auth/screens/login_screen.dart';
+import 'package:yeneta_tutor/utils/colors.dart';
 
 class StudentSignup extends ConsumerStatefulWidget {
   @override
@@ -38,17 +40,17 @@ class _StudentSignupState extends ConsumerState<StudentSignup> {
       );
 
       ref.read(signupControllerProvider).signupStudent(
-        student,
-        _emailController.text,
-        _passwordController.text,
-      );
+            student,
+            _emailController.text,
+            _passwordController.text,
+          );
     }
   }
 
   void _nextPage() {
     if (_currentPage < 1) {
       _pageController.nextPage(
-        duration: Duration(milliseconds: 300),
+        duration: const Duration(milliseconds: 300),
         curve: Curves.easeInOut,
       );
       setState(() {
@@ -60,7 +62,7 @@ class _StudentSignupState extends ConsumerState<StudentSignup> {
   void _previousPage() {
     if (_currentPage > 0) {
       _pageController.previousPage(
-        duration: Duration(milliseconds: 300),
+        duration: const Duration(milliseconds: 300),
         curve: Curves.easeInOut,
       );
       setState(() {
@@ -69,12 +71,13 @@ class _StudentSignupState extends ConsumerState<StudentSignup> {
     }
   }
 
-  InputDecoration _inputDecoration(String label) {
+  InputDecoration _inputDecoration(String label, IconData icon) {
     return InputDecoration(
       labelText: label,
+      prefixIcon: Icon(icon),
       border: OutlineInputBorder(
         borderRadius: BorderRadius.circular(12.0), // Rounded corners
-        borderSide: BorderSide(color: Colors.blue),
+        borderSide: const BorderSide(color: Colors.blue),
       ),
     );
   }
@@ -84,129 +87,189 @@ class _StudentSignupState extends ConsumerState<StudentSignup> {
     return Scaffold(
       appBar: AppBar(title: Text('Student Signup')),
       body: SafeArea(
-        child: SingleChildScrollView( // Add scrolling to prevent overflow
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                // Log or instructions on top
-                Image.asset('images/logo.jpg', height: 200),
-                Text(
-                  _currentPage == 0
-                      ? "Step 1: Fill in your basic information"
-                      : "Step 2: Provide additional details",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              // Log or instructions on top
+              Image.asset('images/logo.jpg', height: 150),
+              Text(
+                _currentPage == 0
+                    ? "Step 1: Fill in your basic information"
+                    : "Step 2: Provide additional details",
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
                 ),
-                SizedBox(height: 16),
+              ),
+              const SizedBox(height: 16),
 
-                // Form fields inside PageView
-                Form(
+              // Form fields inside PageView
+              Expanded(
+                child: Form(
                   key: _formKey,
-                  child: SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.7, // Adjust the height to fit within screen
-                    child: PageView(
-                      controller: _pageController,
-                      physics: NeverScrollableScrollPhysics(), // Disable swipe gestures
-                      children: [
-                        // Page 1
-                        Column(
-                          children: [
-                            TextFormField(
-                              controller: _emailController,
-                              decoration: _inputDecoration('Email'),
-                              validator: (value) =>
-                                  value!.isEmpty ? 'Enter your email' : null,
-                            ),
-                            SizedBox(height: 16),
-                            TextFormField(
-                              controller: _passwordController,
-                              decoration: _inputDecoration('Password'),
-                              obscureText: true,
-                              validator: (value) =>
-                                  value!.isEmpty ? 'Enter your password' : null,
-                            ),
-                            SizedBox(height: 16),
-                            TextFormField(
-                              controller: _nameController,
-                              decoration: _inputDecoration('Name'),
-                              validator: (value) =>
-                                  value!.isEmpty ? 'Enter your name' : null,
-                            ),
-                            SizedBox(height: 16),
-                            TextFormField(
-                              controller: _ageController,
-                              decoration: _inputDecoration('Age'),
-                              keyboardType: TextInputType.number,
-                              validator: (value) =>
-                                  value!.isEmpty ? 'Enter your age' : null,
-                            ),
-                          ],
-                        ),
-                        // Page 2
-                        Column(
-                          children: [
-                            TextFormField(
-                              controller: _sexController,
-                              decoration: _inputDecoration('Sex'),
-                            ),
-                            SizedBox(height: 16),
-                            TextFormField(
-                              controller: _phoneNumberController,
-                              decoration: _inputDecoration('Phone Number'),
-                            ),
-                            SizedBox(height: 16),
-                            TextFormField(
-                              controller: _addressController,
-                              decoration: _inputDecoration('Address'),
-                            ),
-                            SizedBox(height: 16),
-                            TextFormField(
-                              controller: _gradeController,
-                              decoration: _inputDecoration('Grade'),
-                            ),
-                            SizedBox(height: 16),
-                            TextFormField(
-                              controller: _subjectsController,
-                              decoration: _inputDecoration('Subjects of Interest'),
-                            ),
-                            SizedBox(height: 16),
-                            TextFormField(
-                              controller: _languagesController,
-                              decoration: _inputDecoration('Languages Spoken'),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
+                  child: PageView(
+                    controller: _pageController,
+                    physics:
+                        NeverScrollableScrollPhysics(), // Disable swipe gestures
+                    children: [
+                      // Page 1
+                      ListView(
+                        shrinkWrap: true,
+                        children: [
+                          TextFormField(
+                            controller: _emailController,
+                            decoration: _inputDecoration('Email', Icons.email),
+                            validator: (value) =>
+                                value!.isEmpty ? 'Enter your email' : null,
+                          ),
+                          SizedBox(height: 16),
+                          TextFormField(
+                            controller: _passwordController,
+                            decoration:
+                                _inputDecoration('Password', Icons.lock),
+                            obscureText: true,
+                            validator: (value) =>
+                                value!.isEmpty ? 'Enter your password' : null,
+                          ),
+                          const SizedBox(height: 16),
+                          TextFormField(
+                            controller: _nameController,
+                            decoration: _inputDecoration('Name', Icons.person),
+                            validator: (value) =>
+                                value!.isEmpty ? 'Enter your name' : null,
+                          ),
+                          const SizedBox(height: 16),
+                          TextFormField(
+                            controller: _ageController,
+                            decoration:
+                                _inputDecoration('Age', Icons.calendar_today),
+                            keyboardType: TextInputType.number,
+                            validator: (value) =>
+                                value!.isEmpty ? 'Enter your age' : null,
+                          ),
+                        ],
+                      ),
+                      // Page 2
+                      ListView(
+                        shrinkWrap: true,
+                        children: [
+                          TextFormField(
+                            controller: _sexController,
+                            decoration: _inputDecoration('Sex', Icons.person),
+                          ),
+                          const SizedBox(height: 16),
+                          TextFormField(
+                            controller: _phoneNumberController,
+                            decoration:
+                                _inputDecoration('Phone Number', Icons.phone),
+                          ),
+                          const SizedBox(height: 16),
+                          TextFormField(
+                            controller: _addressController,
+                            decoration:
+                                _inputDecoration('Address', Icons.location_on),
+                          ),
+                          const SizedBox(height: 16),
+                          TextFormField(
+                            controller: _gradeController,
+                            decoration: _inputDecoration('Grade', Icons.school),
+                          ),
+                          const SizedBox(height: 16),
+                          TextFormField(
+                            controller: _subjectsController,
+                            decoration: _inputDecoration(
+                                'Subjects of Interest', Icons.book),
+                          ),
+                          const SizedBox(height: 16),
+                          TextFormField(
+                            controller: _languagesController,
+                            decoration: _inputDecoration(
+                                'Languages Spoken', Icons.language),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
-
-                // Navigation Buttons below the form
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    if (_currentPage > 0) // Show 'Previous' only if not on the first page
-                      ElevatedButton(
-                        onPressed: _previousPage,
-                        child: Text('Previous'),
+              ),
+              const SizedBox( height: 16,),
+              // Navigation Buttons at the bottom
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      if (_currentPage >
+                          0) // Show 'Previous' only if not on the first page
+                        TextButton(
+                          onPressed: _previousPage,
+                          style: TextButton.styleFrom(
+                            foregroundColor: Colors.white,
+                            backgroundColor: primaryColor,
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 24, vertical: 12),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          child: const Text('Previous'),
+                        ),
+                      const SizedBox(width: 16),
+                      if (_currentPage <
+                          1) // Show 'Next' if not on the last page
+                        TextButton(
+                          onPressed: _nextPage,
+                          style: TextButton.styleFrom(
+                            foregroundColor: Colors.white,
+                            backgroundColor: primaryColor,
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 24, vertical: 12),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          child: const Text("Next"),
+                        ),
+                         const SizedBox(width: 16),
+                      if (_currentPage == 1) // Show 'Sign Up' on the last page
+                        TextButton(
+                          onPressed: _signupStudent,
+                          style: TextButton.styleFrom(
+                            foregroundColor: Colors.white,
+                            backgroundColor: primaryColor,
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 24, vertical: 12),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          child: const Text('Sign Up'),
+                        ),
+                      const SizedBox(
+                        height: 10,
                       ),
-                    if (_currentPage < 1) // Show 'Next' if not on the last page
-                      ElevatedButton(
-                        onPressed: _nextPage,
-                        child: Text('Next'),
+                    ],
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => LoginScreen()),
+                      );
+                    },
+                    child: const Text(
+                      "Already have an account? Log in",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        decoration: TextDecoration.underline,
                       ),
-                    if (_currentPage == 1) // Show 'Sign Up' on the last page
-                      ElevatedButton(
-                        onPressed: _signupStudent,
-                        child: Text('Sign Up'),
-                      ),
-                  ],
-                ),
-              ],
-            ),
+                    ),
+                  )
+                ],
+              ),
+            ],
           ),
         ),
       ),
