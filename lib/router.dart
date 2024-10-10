3726bc843c93557247d5d228c28a6f260664588e
EphriamHab <ephremhabtamu2015@gmail.com>
2024-10-10 11:33:49 +0300

    router

---
+++ lib/router.dart
@@ -0,0 +1,16 @@
+
+// import 'package:flutter/material.dart';
+// import 'package:yeneta_tutor/features/auth/screens/login_screen.dart';
+// class AppRouter {
+//   static Route<dynamic> generateRoute(RouteSettings settings) {
+//     switch(settings.name) {
+//       case LoginScreen.routeName:
+
+//         return MaterialPageRoute(builder: (_) => LoginScreen());
+//       case StudentSignUpPage1.routeName:
+//         return MaterialPageRoute(builder: (_) => StudentSignUpPage1());
+//       case StudentSignUpPage2.routeName:
+
+//     }
+//   }
+// }
