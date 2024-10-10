f8e393aacf88c98119443461a447f2d406150d92
EphriamHab <ephremhabtamu2015@gmail.com>
2024-10-10 11:35:54 +0300

    splash screen

---
+++ lib/screens/splashScreen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:yeneta_tutor/screens/onboardingScreen1.dart';
+
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    // Set a timer for 2 seconds and then navigate to the next page
+    Timer(const Duration(seconds: 8), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => OnboardingScreen1()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF081C4A), 
+      body: Center(
+        child: Image.asset(
+          'images/yeneta_logo.jpg', 
+          width: 120,
+          height: 120,
+        ),
+      ),
+    );
+  }
+}
