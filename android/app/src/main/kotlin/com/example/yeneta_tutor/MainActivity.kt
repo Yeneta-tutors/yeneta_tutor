4128689089031f992b7ba9ef01ac33ace134f79e
EphriamHab <ephremhabtamu2015@gmail.com>
2024-10-14 22:29:23 +0300

    for vedio explayer

--- android/app/src/main/kotlin/com/example/yeneta_tutor/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/yeneta_tutor/MainActivity.kt
@@ -1,5 +1,23 @@
-package com.example.yeneta_tutor
+package com.example.your_app_name
 
+import android.os.Bundle
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.DefaultRenderersFactory
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Initialize ExoPlayer with fallback
+        val renderersFactory = DefaultRenderersFactory(this)
+            .setEnableDecoderFallback(true)
+
+        val exoPlayer = ExoPlayer.Builder(this)
+            .setRenderersFactory(renderersFactory)
+            .build()
+
+        // You can now use the exoPlayer instance in your app
+    }
+}
