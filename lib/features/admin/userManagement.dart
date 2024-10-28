69013742f6606f4794afdb8dfa0923acf3ac80e2
EphriamHab <ephremhabtamu2015@gmail.com>
2024-10-28 22:36:45 +0300

    user mangement

--- lib/features/admin/userManagement.dart
+++ lib/features/admin/userManagement.dart
@@ -1,15 +1,28 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:yeneta_tutor/features/admin/userProfile.dart';
+import 'package:yeneta_tutor/features/auth/controllers/auth_controller.dart';
+import 'package:yeneta_tutor/models/user_model.dart';
 
-class UserManagement extends StatefulWidget {
+class UserManagement extends ConsumerStatefulWidget {
   @override
   _UserManagementState createState() => _UserManagementState();
 }
 
-class _UserManagementState extends State<UserManagement>
+class _UserManagementState extends ConsumerState<UserManagement>
     with SingleTickerProviderStateMixin {
   late AnimationController _animationController;
   late Animation<double> _animation;
+
+  final TextEditingController _searchController = TextEditingController();
+  String selectedRole = 'All';
+
+  static const roleDisplayNames = {
+    UserRole.student: 'Student',
+    UserRole.tutor: 'Tutor',
+    UserRole.admin: 'Admin',
+  };
 
   @override
   void initState() {
@@ -35,30 +48,41 @@ class _UserManagementState extends State<UserManagement>
   @override
   void dispose() {
     _animationController.dispose();
+    _searchController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    final userStream = ref.watch(userDataAuthProvider);
+    final userData = ref.watch(allUsersProvider);
+    final userStatistics = ref.watch(userStatisticsProvider);
+
     return Scaffold(
       backgroundColor: Color.fromRGBO(243, 242, 247, 1),
       appBar: AppBar(
-        iconTheme: IconThemeData(
-         color: const Color.fromARGB(255, 255, 255, 255)),
+        iconTheme:
+            IconThemeData(color: const Color.fromARGB(255, 255, 255, 255)),
         title: Text('User Management'),
         actions: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
+          userStream.when(
+            data: (user) => Row(
               children: [
-                Text('Hello, Esubalew', style: TextStyle(fontSize: 16)),
+                Text(
+                  user != null ? 'Hello, ${user.firstName}' : 'Hello, User',
+                  style: TextStyle(fontSize: 16),
+                ),
                 SizedBox(width: 10),
                 CircleAvatar(
-                  backgroundImage: AssetImage('images/avator_image.png'),
+                  backgroundImage: user != null && user.profileImage != null
+                      ? NetworkImage(user.profileImage!)
+                      : AssetImage('images/avatar_image.png') as ImageProvider,
                 ),
               ],
             ),
-          )
+            loading: () => CircularProgressIndicator(),
+            error: (error, stack) => Text('Error: $error'),
+          ),
         ],
         backgroundColor: Colors.transparent,
         elevation: 0,
@@ -92,7 +116,7 @@ class _UserManagementState extends State<UserManagement>
                             // Search bar and Filter in the same container as table
                             _buildSearchAndFilterBar(),
                             SizedBox(height: 10),
-                            _buildUserTable(),
+                            _buildUserTable(userData), // User table
                           ],
                         ),
                       ),
@@ -103,7 +127,7 @@ class _UserManagementState extends State<UserManagement>
                       flex: 1,
                       child: Column(
                         children: [
-                          _buildDonutChart(), // Users chart
+                          _buildDonutChart(userStatistics), // Users chart
                           SizedBox(height: 20),
                           _buildGradeChart(), // Students chart with animation
                         ],
@@ -134,37 +158,40 @@ class _UserManagementState extends State<UserManagement>
               width: 250,
               height: 40,
               child: TextField(
+                controller: _searchController,
                 decoration: InputDecoration(
                   labelText: 'Find users',
                   labelStyle: TextStyle(color: Colors.grey, fontSize: 13),
                   contentPadding: EdgeInsets.all(1),
                   border: OutlineInputBorder(),
                 ),
+                onChanged: (_) => setState(() {}),
               ),
             ),
             SizedBox(width: 40),
             PopupMenuButton<String>(
               onSelected: (value) {
-                print('Selected filter: $value');
+                setState(() {
+                  selectedRole = value;
+                });
               },
               icon: Icon(Icons.filter_list),
-              itemBuilder: (BuildContext context) =>
-                  <PopupMenuEntry<String>>[
+              itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
                 PopupMenuItem<String>(
                   value: 'All',
                   child: Text('All'),
                 ),
                 PopupMenuItem<String>(
-                  value: 'Students',
-                  child: Text('Students'),
+                  value: roleDisplayNames[UserRole.student]!, // 'Student'
+                  child: Text(roleDisplayNames[UserRole.student]!),
                 ),
                 PopupMenuItem<String>(
-                  value: 'Tutors',
-                  child: Text('Tutors'),
+                  value: roleDisplayNames[UserRole.tutor]!, // 'Tutor'
+                  child: Text(roleDisplayNames[UserRole.tutor]!),
                 ),
                 PopupMenuItem<String>(
-                  value: 'Admins',
-                  child: Text('Admins'),
+                  value: roleDisplayNames[UserRole.admin]!, // 'Admin'
+                  child: Text(roleDisplayNames[UserRole.admin]!),
                 ),
               ],
             ),
@@ -175,60 +202,90 @@ class _UserManagementState extends State<UserManagement>
   }
 
   // Widget for the User Table
-  Widget _buildUserTable() {
-    return Expanded(
-      child: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: DataTable(
-          columns: [
-            DataColumn(
-                label: Text('ID', style: TextStyle(fontWeight: FontWeight.bold))),
-            DataColumn(
-                label: Text('Full Name', style: TextStyle(fontWeight: FontWeight.bold))),
-            DataColumn(
-                label: Text('Gender', style: TextStyle(fontWeight: FontWeight.bold))),
-            DataColumn(
-                label: Text('Phone Number', style: TextStyle(fontWeight: FontWeight.bold))),
-            DataColumn(
-                label: Text('Role', style: TextStyle(fontWeight: FontWeight.bold))),
-            DataColumn(
-                label: Text('Profile', style: TextStyle(fontWeight: FontWeight.bold))),
-          ],
-          rows: List<DataRow>.generate(
-            15,
-            (index) => DataRow(cells: [
-              DataCell(Text('$index')),
-              DataCell(Text('John Doe')),
-              DataCell(Text('Male')),
-              DataCell(Text('(123) 456-7890')),
-              DataCell(Text('Student')),
-              DataCell(
-                ElevatedButton(
-                  onPressed: () {
-                    // Navigate to profile
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 158, 250, 161),
-                    minimumSize: Size(50, 40),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(7),
-                      side: BorderSide(
-                          color: const Color.fromARGB(255, 116, 111, 111),
-                          width: 1),
+  Widget _buildUserTable(AsyncValue<List<UserModel>> userData) {
+    return userData.when(
+      data: (users) {
+        final searchTerm = _searchController.text.toLowerCase();
+        final filteredUsers = users.where((user) {
+          final matchesRole = selectedRole == 'All' ||
+              (roleDisplayNames[user.role]?.toLowerCase() ==
+                  selectedRole.toLowerCase());
+          final matchesSearch = '${user.firstName} ${user.fatherName}'
+              .toLowerCase()
+              .contains(searchTerm);
+          return matchesRole && matchesSearch;
+        }).toList();
+        return Expanded(
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: DataTable(
+              columns: [
+                DataColumn(
+                    label: Text('ID',
+                        style: TextStyle(fontWeight: FontWeight.bold))),
+                DataColumn(
+                    label: Text('Full Name',
+                        style: TextStyle(fontWeight: FontWeight.bold))),
+                DataColumn(
+                    label: Text('Gender',
+                        style: TextStyle(fontWeight: FontWeight.bold))),
+                DataColumn(
+                    label: Text('Phone Number',
+                        style: TextStyle(fontWeight: FontWeight.bold))),
+                DataColumn(
+                    label: Text('Role',
+                        style: TextStyle(fontWeight: FontWeight.bold))),
+                DataColumn(
+                    label: Text('Profile',
+                        style: TextStyle(fontWeight: FontWeight.bold))),
+              ],
+              rows: filteredUsers.asMap().entries.map((entry) {
+                final index = entry.key + 1;
+                final user = entry.value;
+                return DataRow(cells: [
+                  DataCell(Text("$index")),
+                  DataCell(Text('${user.firstName} ${user.fatherName}')),
+                  DataCell(Text(user.gender)),
+                  DataCell(Text("0${user.phoneNumber}")),
+                  DataCell(Text(roleDisplayNames[user.role] ?? 'Unknown')),
+                  DataCell(
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    UserProfile(userId: user.uid)));
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor:
+                            const Color.fromARGB(255, 158, 250, 161),
+                        minimumSize: Size(50, 40),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(7),
+                          side: BorderSide(
+                            color: const Color.fromARGB(255, 116, 111, 111),
+                            width: 1,
+                          ),
+                        ),
+                      ),
+                      child: Text('View Profile'),
                     ),
                   ),
-                  child: Text('View Profile'),
-                ),
-              ),
-            ]),
+                ]);
+              }).toList(),
+            ),
           ),
-        ),
-      ),
+        );
+      },
+      loading: () => Center(child: CircularProgressIndicator()),
+      error: (error, stack) =>
+          Center(child: Text('Failed to load users: $error')),
     );
   }
 
   // Widget for Donut Chart
-  Widget _buildDonutChart() {
+  Widget _buildDonutChart(AsyncValue<Map<UserRole, int>> userStatistics) {
     return Container(
       padding: EdgeInsets.all(16.0),
       decoration: BoxDecoration(
@@ -242,51 +299,77 @@ class _UserManagementState extends State<UserManagement>
           ),
         ],
       ),
-      child: Column(
-        children: [
-          Text(
-            'Users',
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 10),
-          SizedBox(
-            width: double.infinity,
-            height: 200,
-            child: PieChart(
-              PieChartData(
-                sections: [
-                  PieChartSectionData(
+      child: userStatistics.when(
+        data: (stats) {
+          int totalUsers = stats.values.fold(0, (a, b) => a + b);
+
+          return Column(
+            children: [
+              Text(
+                'Users',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10),
+              SizedBox(
+                width: double.infinity,
+                height: 200,
+                child: PieChart(
+                  PieChartData(
+                    sections: [
+                      PieChartSectionData(
+                        color: Colors.blue,
+                        value: (stats[UserRole.student]! / totalUsers) *
+                            100, // Change to your dynamic value
+                        title: '${stats[UserRole.student]}',
+                        radius: 30,
+                      ),
+                      PieChartSectionData(
+                        color: Colors.red,
+                        value: (stats[UserRole.tutor]! / totalUsers) *
+                            100, // Change to your dynamic value
+                        title: '${stats[UserRole.tutor]}',
+                        radius: 30,
+                      ),
+                      PieChartSectionData(
+                        color: Colors.green,
+                        value: (stats[UserRole.admin]! / totalUsers) *
+                            100, // Change to your dynamic value
+                        title: '${stats[UserRole.admin]}',
+                        radius: 30,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  _buildIndicator(
                     color: Colors.blue,
-                    value: 40, // Change to your dynamic value
-                    title: '66',
-                    radius: 30,
+                    text: 'Students',
+                    percentage:
+                        '${((stats[UserRole.student]! / totalUsers) * 100).toStringAsFixed(1)}%',
                   ),
-                  PieChartSectionData(
+                  _buildIndicator(
                     color: Colors.red,
-                    value: 30, // Change to your dynamic value
-                    title: '14',
-                    radius: 30,
+                    text: 'Tutors',
+                    percentage:
+                        '${((stats[UserRole.tutor]! / totalUsers) * 100).toStringAsFixed(1)}%',
                   ),
-                  PieChartSectionData(
+                  _buildIndicator(
                     color: Colors.green,
-                    value: 30, // Change to your dynamic value
-                    title: '12',
-                    radius: 30,
+                    text: 'Admins',
+                    percentage:
+                        '${((stats[UserRole.admin]! / totalUsers) * 100).toStringAsFixed(1)}%',
                   ),
                 ],
               ),
-            ),
-          ),
-          SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              _buildIndicator(color: Colors.blue, text: 'Students', percentage: '18'),
-              _buildIndicator(color: Colors.red, text: 'Tutors', percentage: '15'),
-              _buildIndicator(color: Colors.green, text: 'Admins', percentage: '12'),
             ],
-          ),
-        ],
+          );
+        },
+        loading: () => CircularProgressIndicator(),
+        error: (err, stack) => Text('Error: $err'),
       ),
     );
   }
@@ -321,7 +404,8 @@ class _UserManagementState extends State<UserManagement>
                     sections: [
                       PieChartSectionData(
                         color: Colors.blueAccent,
-                        value: 30 * _animation.value, // Multiply by animation value
+                        value: 30 *
+                            _animation.value, // Multiply by animation value
                         title: '${(30 * _animation.value).toStringAsFixed(1)}%',
                         radius: 30,
                       ),
@@ -351,13 +435,25 @@ class _UserManagementState extends State<UserManagement>
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  _buildIndicator(color: Colors.blueAccent, text: 'Grade 9', percentage: '30%'),
+                  _buildIndicator(
+                      color: Colors.blueAccent,
+                      text: 'Grade 9',
+                      percentage: '30%'),
                   SizedBox(height: 10),
-                  _buildIndicator(color: Colors.orangeAccent, text: 'Grade 10', percentage: '25%'),
+                  _buildIndicator(
+                      color: Colors.orangeAccent,
+                      text: 'Grade 10',
+                      percentage: '25%'),
                   SizedBox(height: 10),
-                  _buildIndicator(color: Colors.greenAccent, text: 'Grade 11', percentage: '45%'),
+                  _buildIndicator(
+                      color: Colors.greenAccent,
+                      text: 'Grade 11',
+                      percentage: '45%'),
                   SizedBox(height: 10),
-                  _buildIndicator(color: Color.fromARGB(255, 136, 255, 0), text: 'Grade 12', percentage: '45%'),
+                  _buildIndicator(
+                      color: Color.fromARGB(255, 136, 255, 0),
+                      text: 'Grade 12',
+                      percentage: '45%'),
                 ],
               ),
             ],
@@ -368,7 +464,10 @@ class _UserManagementState extends State<UserManagement>
   }
 
   // Helper widget to build chart indicators
-  Widget _buildIndicator({required Color color, required String text, required String percentage}) {
+  Widget _buildIndicator(
+      {required Color color,
+      required String text,
+      required String percentage}) {
     return Row(
       children: [
         Container(
