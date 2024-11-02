a6c77d8ad8e733ddee1cf82f3dfbd10f698fd9a2
EphriamHab <ephremhabtamu2015@gmail.com>
2024-11-02 22:47:28 +0300

    add logout

--- lib/features/admin/admin_sidebar.dart
+++ lib/features/admin/admin_sidebar.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:yeneta_tutor/features/admin/addAdmin.dart';
 import 'package:yeneta_tutor/features/admin/dashboard.dart';
 import 'package:yeneta_tutor/features/admin/userManagement.dart';
 import 'package:yeneta_tutor/features/admin/contentManagement.dart';
+import 'package:yeneta_tutor/features/auth/controllers/auth_controller.dart';
+import 'package:yeneta_tutor/features/auth/screens/login_screen.dart';
 
-class Sidebar extends StatelessWidget {
+class Sidebar extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
@@ -23,24 +26,56 @@ class Sidebar extends StatelessWidget {
               ),
             ),
           ),
-          SidebarItem(title: 'Dashboard', icon: Icons.dashboard, onTap: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) => AdminDashboard()));
-          }),
-          SidebarItem(title: 'User Management', icon: Icons.people, onTap: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) => UserManagement()));
-          }),
-          SidebarItem(title: 'Content Management', icon: Icons.video_library, onTap: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) => ContentManagementPage()));
-          }),
-          SidebarItem(title: 'Financial Management', icon: Icons.attach_money, onTap: () {
-            // Navigator.push(context, MaterialPageRoute(builder: (context) => FinancialManagementPage()));
-          }),
-          SidebarItem(title: 'Reports', icon: Icons.bar_chart, onTap: () {
-            // Navigator.push(context, MaterialPageRoute(builder: (context) => ReportsPage()));
-          }),
-          SidebarItem(title: 'Add Admin', icon: Icons.add, onTap: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) => AddAdminPage()));
-          }),
+          SidebarItem(
+              title: 'Dashboard',
+              icon: Icons.dashboard,
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => AdminDashboard()));
+              }),
+          SidebarItem(
+              title: 'User Management',
+              icon: Icons.people,
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => UserManagement()));
+              }),
+          SidebarItem(
+              title: 'Content Management',
+              icon: Icons.video_library,
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => ContentManagementPage()));
+              }),
+          // SidebarItem(title: 'Financial Management', icon: Icons.attach_money, onTap: () {
+          //   Navigator.push(context, MaterialPageRoute(builder: (context) => FinancialManagementPage()));
+          // }),
+          // SidebarItem(title: 'Reports', icon: Icons.bar_chart, onTap: () {
+          //   Navigator.push(context, MaterialPageRoute(builder: (context) => ReportsPage()));
+          // }),
+          SidebarItem(
+              title: 'Add Admin',
+              icon: Icons.add,
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => AddAdminPage()));
+              }),
+
+          SidebarItem(
+            title: 'Logout',
+            icon: Icons.logout,
+            onTap: () {
+              ref.read(authControllerProvider).signOut();
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => LoginScreen(),
+                ),
+              );
+            },
+          )
         ],
       ),
     );
